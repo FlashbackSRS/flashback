f56d93a02e75b13f0367653cb0273b2e04a26e42
Jonathan Hall <flimzy@flimzy.com>
2017-02-06 15:56:26 +0100

    Update cloze tests

--- controllers/anki/cloze_test.go
+++ controllers/anki/cloze_test.go
@@ -1,14 +1,12 @@
 package anki
 
 import (
-	"bytes"
-	"fmt"
 	"html/template"
 	"testing"
 )
 
 type clozeTest struct {
-	TemplateID int
+	TemplateID uint32
 	Face       int
 	Text       template.HTML
 	Expected   template.HTML
@@ -17,70 +15,47 @@ type clozeTest struct {
 func TestCloze(t *testing.T) {
 	tests := []clozeTest{
 		clozeTest{
-			TemplateID: 1,
+			TemplateID: 0,
 			Face:       0,
 			Text:       "Quien mucho {{c1::abarca}} poco {{c2::aprieta}}.",
 			Expected:   `Quien mucho <span class="cloze">[...]</span> poco aprieta.`,
 		},
 		clozeTest{
-			TemplateID: 2,
+			TemplateID: 1,
 			Face:       0,
 			Text:       "Quien mucho {{c1::abarca}} poco {{c2::aprieta}}.",
 			Expected:   `Quien mucho abarca poco <span class="cloze">[...]</span>.`,
 		},
 		clozeTest{
-			TemplateID: 3,
+			TemplateID: 2,
 			Face:       0,
 			Text:       "Quien mucho {{c1::abarca}} poco {{c2::aprieta}}.",
 			Expected:   "",
 		},
 		clozeTest{
-			TemplateID: 1,
+			TemplateID: 0,
 			Face:       1,
 			Text:       "Quien mucho {{c1::abarca}} poco {{c2::aprieta}}.",
 			Expected:   `Quien mucho <span class="cloze">abarca</span> poco aprieta.`,
 		},
 		clozeTest{
-			TemplateID: 2,
+			TemplateID: 1,
 			Face:       1,
 			Text:       "Quien mucho {{c1::abarca}} poco {{c2::aprieta}}.",
 			Expected:   `Quien mucho abarca poco <span class="cloze">aprieta</span>.`,
 		},
 		clozeTest{
-			TemplateID: 3,
+			TemplateID: 2,
 			Face:       1,
 			Text:       "Quien mucho {{c1::abarca}} poco {{c2::aprieta}}.",
 			Expected:   "",
 		},
 	}
 	for _, test := range tests {
-		fn := cloze(test.Face, test.TemplateID)
+		fn := cloze(test.TemplateID, test.Face)
 		result := fn(test.Text)
 		if result != test.Expected {
-			t.Errorf("Card %d, Face %d, %s\n\tExpected: %s\n\t  Actual: %s\n", test.TemplateID, test.Face, test.Text, test.Expected, result)
+			t.Errorf("Template %d, Face %d, %s\n\tExpected: %s\n\t  Actual: %s\n", test.TemplateID, test.Face, test.Text, test.Expected, result)
 		}
 	}
-}
-
-type card struct{}
-
-func (c *card) ModelID() int {
-	return 1
-}
-
-func TestClozeTemplate(t *testing.T) {
-	tmpl, err := template.New("template").Funcs(map[string]interface{}{"cloze": cloze}).Parse("{{cloze .Face .Card.ModelID .Foo}}")
-	if err != nil {
-		t.Fatalf("error parsing template: %s\n", err)
-	}
-	ctx := map[string]interface{}{
-		"Face": 1,
-		"Card": &card{},
-		"Foo":  "Foo {{c1::bar}} baz",
-	}
-	result := new(bytes.Buffer)
-	if err := tmpl.Execute(result, ctx); err != nil {
-		t.Fatalf("Error executing template: %s\n", err)
-	}
-	fmt.Printf("Result = %s\n", result.String())
 }
