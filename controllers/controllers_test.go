408c654ffd8ade5c6c990ff147723be4c0c17901
Jonathan Hall <flimzy@flimzy.com>
2017-08-05 20:24:41 +0200

    Move model controller logic to controllers package

---
+++ controllers/controllers_test.go
@@ -0,0 +1,74 @@
+package controllers
+
+import (
+	"testing"
+
+	"github.com/flimzy/diff"
+)
+
+type mockMC struct {
+	ModelController
+	t string
+}
+
+func (mc *mockMC) Type() string {
+	return mc.t
+}
+
+const testMType = "foo"
+
+var testMC = &mockMC{t: testMType}
+
+func TestRegisterModelController(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		RegisterModelController(testMC)
+		if _, ok := modelControllers[testMType]; !ok {
+			t.Errorf("modelControllers not updated")
+		}
+		for _, t := range modelControllerTypes {
+			if t == testMType {
+				return
+			}
+		}
+		t.Errorf("modelControllerTypes not updated")
+	})
+	t.Run("Duplicate", func(t *testing.T) {
+		r := func() (r interface{}) {
+			defer func() {
+				r = recover()
+			}()
+			RegisterModelController(testMC)
+			return
+		}()
+		expected := "A controller for 'foo' is already registered"
+		if d := diff.Interface(expected, r); d != "" {
+			t.Errorf(d)
+		}
+	})
+}
+
+func TestRegisteredModelControllers(t *testing.T) {
+	expected := []string{"foo"}
+	result := RegisteredModelControllers()
+	if d := diff.Interface(expected, result); d != "" {
+		t.Error(d)
+	}
+}
+
+func TestGetModelController(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		mc, err := GetModelController(testMType)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if mc != testMC {
+			t.Errorf("Unexpected controller returned")
+		}
+	})
+	t.Run("Not found", func(t *testing.T) {
+		_, err := GetModelController("not found")
+		if err == nil || err.Error() != "ModelController for 'not found' not found" {
+			t.Errorf("Unexpected error: %s", err)
+		}
+	})
+}
