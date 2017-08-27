2d59b13048765172b71639783da5e62eaa00a2e5
Jonathan Hall <flimzy@flimzy.com>
2017-08-27 14:13:08 +0200

    Move anki Basic stuff to basic.go

--- controllers/anki/anki.go
+++ controllers/anki/anki.go
@@ -2,13 +2,9 @@
 package anki
 
 import (
-	"time"
-
 	"github.com/flimzy/log"
-	"github.com/pkg/errors"
 
 	"github.com/FlashbackSRS/flashback"
-	"github.com/FlashbackSRS/flashback/diff"
 	"github.com/FlashbackSRS/flashback/model"
 	"github.com/FlashbackSRS/flashback/webclient/views/studyview"
 )
@@ -19,11 +15,6 @@ const (
 	AnswerFace
 )
 
-// Basic is the controller for the Anki Basic model
-type Basic struct{}
-
-var _ model.ModelController = &Basic{}
-
 func init() {
 	log.Debug("Registering anki models\n")
 	model.RegisterModelController(&Basic{})
@@ -31,21 +22,7 @@ func init() {
 	log.Debug("Done registering anki models\n")
 }
 
-// Type returns the string "anki-basic", to identify this model handler's type.
-func (m *Basic) Type() string {
-	return "anki-basic"
-}
-
 //go:generate go-bindata -pkg anki -nocompress -prefix files -o data.go files
-
-// IframeScript returns JavaScript to run inside the iframe.
-func (m *Basic) IframeScript() []byte {
-	data, err := Asset("script.js")
-	if err != nil {
-		panic(err)
-	}
-	return data
-}
 
 var buttonMaps = map[int]studyview.ButtonMap{
 	QuestionFace: studyview.ButtonMap{
@@ -74,72 +51,9 @@ var buttonMaps = map[int]studyview.ButtonMap{
 	},
 }
 
-// Buttons returns the initial button state
-func (m *Basic) Buttons(face int) (studyview.ButtonMap, error) {
-	buttons, ok := buttonMaps[face]
-	if !ok {
-		return nil, errors.Errorf("Invalid face %d", face)
-	}
-	return buttons, nil
-}
-
 type answer struct {
 	Text    string `json:"text"`
 	Correct bool   `json:"correct"`
-}
-
-// Action responds to a card action, such as a button press
-func (m *Basic) Action(card *model.Card, face *int, startTime time.Time, payload interface{}) (bool, error) {
-	query := convertQuery(payload)
-	log.Debugf("Submit recieved for face %d: %v\n", *face, query)
-	button := studyview.Button(query.Submit)
-	log.Debugf("Button %s pressed\n", button)
-	switch *face {
-	case QuestionFace:
-		// Any input is fine; the only options are the right button, or 'ENTER' in a text field.
-	case AnswerFace:
-		if _, valid := buttonMaps[*face][button]; !valid {
-			return false, errors.Errorf("Unexpected button press %s", button)
-		}
-	default:
-		return false, errors.Errorf("Unexpected face %d", *face)
-	}
-	switch *face {
-	case QuestionFace:
-		*face++
-		typedAnswers := query.TypedAnswers
-		if len(typedAnswers) > 0 {
-			results := make(map[string]answer)
-			for _, fieldName := range card.Fields() {
-				if typedAnswer, ok := typedAnswers[fieldName]; ok {
-					fv := card.FieldValue(fieldName)
-					if fv == nil {
-						panic("No field value for field")
-					}
-					correct, d := diff.Diff(fv.Text, typedAnswer)
-					results[fieldName] = answer{
-						Text:    d,
-						Correct: correct,
-					}
-				}
-			}
-			card.Context = map[string]interface{}{
-				"typedAnswers": results,
-			}
-			return true, nil
-		}
-		return false, nil
-	case AnswerFace:
-		log.Debugf("Old schedule: Due %s, Interval: %s, Ease: %f, ReviewCount: %d\n", card.Due, card.Interval, card.EaseFactor, card.ReviewCount)
-		if err := model.Schedule(card, time.Now().Sub(startTime), quality(button)); err != nil {
-			return false, err
-		}
-		log.Debugf("New schedule: Due %s, Interval: %s, Ease: %f, ReviewCount: %d\n", card.Due, card.Interval, card.EaseFactor, card.ReviewCount)
-		card.Context = nil // Clear any saved answers
-		return true, nil
-	}
-	log.Printf("Unexpected face/button combo: %d / %+v\n", *face, button)
-	return false, nil
 }
 
 func quality(button studyview.Button) flashback.AnswerQuality {
